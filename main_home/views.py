daa64dd6a1a8ca9e436dd4112fc4fc4674e944c8
Armanghz <cybertechik@protonmail.com>
2023-05-19 01:09:17 +0330

    created signup and login backend using django

--- main_home/views.py
+++ main_home/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import logout
 
 def home(request):
     return render(request, 'home/index.html')
 def menu(request):
     return render(request, 'home/other/menu.html')
+def logout_p(request):
+    logout(request)
+    return redirect('home')
--- users/views.py
+++ users/views.py
@@ -1,10 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
+from django.contrib.auth import logout
 
 def signup_p(request):
+    if request.method == 'POST':
+        name = request.POST['name']
+        email = request.POST['email']
+        password = request.POST['password']
+        
+        # Create a new user
+        user = User.objects.create_user(username=email, email=email, password=password)
+        user.first_name = name
+        user.save()
+        
+        # Redirect to login page or any other desired page
+        return redirect('login')
+    
     return render(request, 'users/other/signup.html')
 def account_p(request):
     return render(request, 'users/account.html')
 def login_p(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        password = request.POST['password']
+        
+        # Authenticate user
+        user = authenticate(request, username=email, password=password)
+        
+        if user is not None:
+            # Login user
+            login(request, user)
+            
+            # Redirect to the desired page
+            return redirect('home')
+        else:
+            error_message = "Invalid email or password."
+            return render(request, 'users/other/login.html', {'error_message': error_message})
+    
     return render(request, 'users/other/login.html')
 def reset_pass_p(request):
     return render(request, 'users/other/resetpass.html')
+

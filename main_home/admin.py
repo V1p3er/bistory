18195b3835f0c2320979e4baeac5feeac1039090
v1p3er <cybertechik@protonmail.com>
2023-10-26 01:59:28 +0330

    sync

--- main_home/admin.py
+++ main_home/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import ShopItem
+
+
+class ShopItemAdmin(admin.ModelAdmin):
+    list_display = ("name", "price", "category")
+    list_filter = ("category",)
+    search_fields = ("name",)
+
+admin.site.register(ShopItem, ShopItemAdmin)
--- main_home/models.py
+++ main_home/models.py
@@ -1,1 +1,7 @@
 from django.db import models
+
+class ShopItem(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.CharField(max_length=255)
+    image = models.ImageField(upload_to="home/img/")
